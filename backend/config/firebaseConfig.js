bd9a0b1d98dfbfd84cbbb3d17d7ce69aaf77a19a
Lavavarshney <lavanya.varshney2104@gmail.com>
2025-02-28 01:57:15 +0530

    removed secrets

--- backend/config/firebaseConfig.js
+++ backend/config/firebaseConfig.js
@@ -1,9 +1,1 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-48be9-default-rtdb.firebaseio.com/", // Replace with your actual Firebase database URL
-});
-
-module.exports = admin;
